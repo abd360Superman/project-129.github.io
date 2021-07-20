b9dc1e49d0622f65adfb2f1c96c5c107c23cafef
abd360superman <darshchoudhary171@gmail.com>
2021-07-20 13:57:37 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,62 @@
+song1 = '';
+song2 = '';
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0
+
+function preload() {
+    song1 = loadSound('music.mp3');
+    song2 = loadSound('music2.mp3');
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, model_load_hogaya);
+    poseNet.on('pose', poses_milgaya);
+}
+
+function model_load_hogaya(){
+    console.log('Posenet model is initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill('#FF0000');
+    stroke('#FF0000');
+
+    if(scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop()
+        if (song1_status === false) {
+            song1.play()
+        }
+        document.getElementById('song').innerHTML = 'Song = Harry Potter Theme Song'
+    }
+}
+
+function poses_milgaya(results){
+    if(results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log('scoreLeftWrist = ' + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log('letfWristX = ' + leftWristX + ', leftWristY = ' + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log('rightWristX = ' + rightWristX + ', rightWristY = ' + rightWristY);
+    }
+}
